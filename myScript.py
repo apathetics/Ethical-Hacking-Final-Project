b47a133822d18c1b6f6eeb4f46287e2fe2671f73
root <root@localhost.localdomain>
2018-11-24 22:27:29 -0500

    added nmap-vulners script

--- myScript.py
+++ myScript.py
@@ -1,5 +1,6 @@
 import nmap
 import sys
+from subprocess import check_output
 
 
 def nmapScan(ipRange = '127.0.0.1', portRange = '1-1000'):
@@ -13,16 +14,22 @@ def nmapScan(ipRange = '127.0.0.1', portRange = '1-1000'):
 	# we can parse this for something? or use vulnscan
 	print(nm.csv())
 
+	result = check_output("nmap --script nmap-vulners -sV " + ipRange, shell=True)
+	print(result)
+
 
 if __name__ =='__main__':
 
 	# step one: doing the nmap scan and parsing the results into dictionary
 	# read in arguments, assume provided as (ip, port) in that order
 	# we can specify arguments like -ip= and -port= respectively later
-	if len(sys.argv) == 1:
+	numArgs = len(sys.argv)
+
+	if numArgs == 1:
 		nmapResults = nmapScan()
 	else:
 		ipRange = sys.argv[1]
-		portRange = sys.argv[2]
+		portRange = sys.argv[2] if numArgs > 2 else "1-1000"
+
 		nmapResults = nmapScan(ipRange, portRange)
 
